3424a3592a92cbaaa832968970a7401be591d128
Tyler Weir <tyler.weir@gmail.com>
2009-12-09 08:16:02 -0500

    Capital letter is a class, huh.

--- Library/Formula/uncrustify.rb
+++ Library/Formula/uncrustify.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
-class uncrustify < Formula
+class Uncrustify < Formula
 
   head 'git://github.com/bengardner/uncrustify.git'
   homepage 'http://uncrustify.sourceforge.net/'
 
   def install
-    system "./configure",  "--prefix=#{prefix}",
+    system "./configure",  "--prefix=#{prefix}"
 
     system "make install"
   end
